827889bfc01e7d1e78be899a257aad49f5e2ab30
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-03-17 17:27:08 -0700

    fix: handle errors according to the interface being used BREAKING CHANGE: Errors are no longer thrown on invalid input.

--- src/loadFileConfig.js
+++ src/loadFileConfig.js
@@ -1,19 +1,18 @@
 // @flow
 import {readFile} from 'fs';
-import pify from 'pify';
 import type {FileConfig} from './types';
-
-const readFileAsync: (
-  filePath: string,
-  encoding: string,
-) => Promise<?string> = pify(readFile);
 
 export default function loadFileConfig(property: FileConfig) {
   const {encoding = 'utf8', filePath, required = false} = property;
 
   // $FlowFixMe
-  return readFileAsync(filePath, encoding).then(
-    config => ({config, error: null}),
-    (error: Error) => ({config: undefined, error: required ? error : null}),
+  return new Promise(resolve =>
+    readFile(filePath, encoding, (error, config) =>
+      resolve({
+        // config: config ?? undefined,
+        config: typeof config === 'string' ? config : undefined,
+        error: error && required ? error : null,
+      }),
+    ),
   );
 }
