95dfa91f6290fd5671322c05d992d0304f30c48e
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-04-21 11:53:05 -0700

    refactor: use build script (#59)

--- scripts/rollup/config.es.js
+++
@@ -1,7 +0,0 @@
-import assignDeep from 'assign-deep';
-import cjsConfig from './config.cjs';
-import pkg from '../../package.json';
-
-export default assignDeep({}, cjsConfig, {
-  output: {file: pkg.module, format: 'es'},
-});
