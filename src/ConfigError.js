433cfc40eb6e619c90c168d6eb1e7e2305bb8e23
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-05-01 17:38:14 -0700

    perf: don't double assign `message` of `ConfigError` (#66)

--- src/ConfigError.js
+++ src/ConfigError.js
@@ -5,7 +5,6 @@ export default class ConfigError extends Error {
   constructor(errors: Array<Error>) {
     super('Configuration could not be loaded.');
     this.errors = errors;
-    this.message = 'Configuration could not be loaded.';
   }
 
   toJSON() {
