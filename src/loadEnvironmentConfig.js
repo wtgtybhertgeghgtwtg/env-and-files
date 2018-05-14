066bc4a5419a5ba643965a119d85daba1c56206f
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-05-13 18:42:48 -0700

    refactor: don't double-check in `loadEnvironmentConfig`

--- src/loadEnvironmentConfig.js
+++ src/loadEnvironmentConfig.js
@@ -3,15 +3,7 @@ import type {EnvironmentConfig} from './types';
 
 export default function loadEnvironmentConfig(property: EnvironmentConfig) {
   const {required = false, variableName} = property;
-
-  // '' is falsey, so `process.env[variableName] || undefined` would not behave right when the variable is set to ''.
-  let config = process.env[variableName];
-
-  // `process.env[variableName] ?? undefined`, on the other hand.
-  if (typeof config !== 'string') {
-    config = undefined;
-  }
-
+  const config = process.env[variableName];
   const error =
     required && typeof config !== 'string'
       ? new Error(`${variableName} is not defined.`)
