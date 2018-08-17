1417cb79934e6ffbf42eba1740a008d255d46d7f
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-08-16 18:39:37 -0700

    fix: make `flow@0.79.0` work

--- src/loadFileConfig.js
+++ src/loadFileConfig.js
@@ -5,7 +5,7 @@ import type {FileConfig} from './types';
 export default function loadFileConfig(property: FileConfig) {
   const {encoding = 'utf8', filePath, required = false} = property;
 
-  return new Promise(resolve =>
+  return new Promise<{error: false | ?Error, value: ?string}>(resolve =>
     readFile(filePath, encoding, (error, value) =>
       resolve({error: required && error, value: value ?? undefined}),
     ),
